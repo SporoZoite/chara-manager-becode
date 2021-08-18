fcb28504474744aafe8e2ffc3fd2869d46183547
SporoZoite <lisavanhooydonk@gmail.com>
2021-08-18 14:56:01 +0200

    clean functions (I think)

--- newchara.js
+++ newchara.js
@@ -1,140 +1,119 @@
 var // where files are dropped + file selector is opened
-	dropRegion = document.getElementById("drop-region"),
-	// where images are previewed
-	imagePreviewRegion = document.getElementById("image-preview");
-
+  dropRegion = document.getElementById("drop-region"),
+  // where images are previewed
+  imagePreviewRegion = document.getElementById("image-preview");
 
 // open file selector when clicked on the drop region
 var fakeInput = document.createElement("input");
 fakeInput.type = "file";
 fakeInput.accept = "image/*";
 fakeInput.multiple = true;
-dropRegion.addEventListener('click', function() {
-	fakeInput.click();
+dropRegion.addEventListener("click", function () {
+  fakeInput.click();
 });
 
-
-fakeInput.addEventListener("change", function() {
-	var files = fakeInput.files;
-	handleFiles(files);
+fakeInput.addEventListener("change", function () {
+  var files = fakeInput.files;
+  handleFiles(files);
 });
-
 
 function preventDefault(e) {
-	e.preventDefault();
-  	e.stopPropagation();
+  e.preventDefault();
+  e.stopPropagation();
 }
 
-dropRegion.addEventListener('dragenter', preventDefault, false)
-dropRegion.addEventListener('dragleave', preventDefault, false)
-dropRegion.addEventListener('dragover', preventDefault, false)
-dropRegion.addEventListener('drop', preventDefault, false)
-
+dropRegion.addEventListener("dragenter", preventDefault, false);
+dropRegion.addEventListener("dragleave", preventDefault, false);
+dropRegion.addEventListener("dragover", preventDefault, false);
+dropRegion.addEventListener("drop", preventDefault, false);
 
 function handleDrop(e) {
-	var dt = e.dataTransfer,
-		files = dt.files;
+  var dt = e.dataTransfer,
+    files = dt.files;
 
-	if (files.length) {
+  if (files.length) {
+    handleFiles(files);
+  } else {
+    // check for img
+    var html = dt.getData("text/html"),
+      match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html),
+      url = match && match[1];
 
-		handleFiles(files);
-		
-	} else {
+    if (url) {
+      uploadImageFromURL(url);
+      return;
+    }
+  }
 
-		// check for img
-		var html = dt.getData('text/html'),
-	        match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html),
-	        url = match && match[1];
+  function uploadImageFromURL(url) {
+    var img = new Image();
+    var c = document.createElement("canvas");
+    var ctx = c.getContext("2d");
 
+    img.onload = function () {
+      c.width = this.naturalWidth; // update canvas size to match image
+      c.height = this.naturalHeight;
+      ctx.drawImage(this, 0, 0); // draw in image
+      c.toBlob(function (blob) {
+        // get content as PNG blob
 
-
-	    if (url) {
-	        uploadImageFromURL(url);
-	        return;
-	    }
-
-	}
-
-
-	function uploadImageFromURL(url) {
-		var img = new Image;
-        var c = document.createElement("canvas");
-        var ctx = c.getContext("2d");
-
-        img.onload = function() {
-            c.width = this.naturalWidth;     // update canvas size to match image
-            c.height = this.naturalHeight;
-            ctx.drawImage(this, 0, 0);       // draw in image
-            c.toBlob(function(blob) {        // get content as PNG blob
-
-            	// call our main function
-                handleFiles( [blob] );
-
-            }, "image/png");
-        };
-        img.onerror = function() {
-            alert("Error in uploading");
-        }
-        img.crossOrigin = "";              // if from different origin
-        img.src = url;
-	}
-
+        // call our main function
+        handleFiles([blob]);
+      }, "image/png");
+    };
+    img.onerror = function () {
+      alert("Error in uploading");
+    };
+    img.crossOrigin = ""; // if from different origin
+    img.src = url;
+  }
 }
 
-dropRegion.addEventListener('drop', handleDrop, false);
-
-
+dropRegion.addEventListener("drop", handleDrop, false);
 
 function handleFiles(files) {
-	for (var i = 0, len = files.length; i < len; i++) {
-		if (validateImage(files[i]))
-			previewImage(files[i]);
-	}
+  for (var i = 0, len = files.length; i < len; i++) {
+    if (validateImage(files[i])) previewImage(files[i]);
+  }
 }
 
 function validateImage(image) {
-	// check the type
-	var validTypes = ['image/jpeg', 'image/png', 'image/gif'];
-	if (validTypes.indexOf( image.type ) === -1) {
-		alert("Invalid File Type");
-		return false;
-	}
+  // check the type
+  var validTypes = ["image/jpeg", "image/png", "image/gif"];
+  if (validTypes.indexOf(image.type) === -1) {
+    alert("Invalid File Type");
+    return false;
+  }
 
-	// check the size
-	var maxSizeInBytes = 10e6; // 10MB
-	if (image.size > maxSizeInBytes) {
-		alert("File too large");
-		return false;
-	}
+  // check the size
+  var maxSizeInBytes = 10e6; // 10MB
+  if (image.size > maxSizeInBytes) {
+    alert("File too large");
+    return false;
+  }
 
-	return true;
-
+  return true;
 }
-
 
 function previewImage(image) {
+  // container
+  var imgView = document.createElement("div");
+  imgView.className = "image-view";
+  imagePreviewRegion.appendChild(imgView);
 
-	// container
-	var imgView = document.createElement("div");
-	imgView.className = "image-view";
-	imagePreviewRegion.appendChild(imgView);
+  // previewing image
+  var img = document.createElement("img");
+  img.setAttribute("id", "prevmage");
+  img.className = "rounded";
+  imgView.appendChild(img);
 
-	// previewing image
-	var img = document.createElement("img");
-    img.setAttribute("id","prevmage");
-    img.className="rounded";
-	imgView.appendChild(img);
-
-
-	// read the image...
-	var reader = new FileReader();
-	reader.onload = function(e) {
-		img.src = e.target.result;
+  // read the image...
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    img.src = e.target.result;
   };
-	reader.readAsDataURL(image);
+  reader.readAsDataURL(image);
 }
-
-
 
 const buttonSolo = document.createElement("div");
 buttonSolo.setAttribute("class", "buttonSolo");
@@ -143,69 +122,69 @@ cardSolo.appendChild(buttonSolo);
 const updateButton = document.createElement("input");
 updateButton.setAttribute("id", "updateButton");
 updateButton.setAttribute("type", "submit");
-updateButton.setAttribute("value", "Save New Character");;
+updateButton.setAttribute("value", "Save New Character");
 buttonSolo.appendChild(updateButton);
-updateButton.addEventListener("click",async() =>{
-    const prevMage = document.getElementById("prevmage");
-    let text = Array.from(document.querySelectorAll("textarea"));
-    let values = text.map(({value})=>{
-        return value.trim();
-    });
-    if (values.some((value) => value === "")) {
-        alert("Please fill in all fields");
-        return;
-      }
-  
-      let [name, shortDescription] = values;
-      console.log(values);
-      let image = prevMage.src;
-      image = image.substr(22);
-      let description = document.getElementById("editor").innerHTML;
-      console.log (description);
-      console.log(image);
-      let id = null;
- 
-  
-      const postData = await fetch("https://character-database.becode.xyz/characters", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id, name, shortDescription, description, image }),
-      });
-      console.log(postData);
-      console.log(postData.json());
-      window.location.href="index.html"
-    });
-  
-  
-
+updateButton.addEventListener("click", async () => {
+  editor();
+});
 
 const deleteButton = document.createElement("button");
 deleteButton.setAttribute("id", "deleteButton");
-deleteButton.innerText = "Delete character";
+deleteButton.innerText = "Cancel character";
 buttonSolo.appendChild(deleteButton);
 deleteButton.addEventListener("click", () => {
-  window.location.href="index.html"
-            });
-
+  back();
+});
 
 const backButton = document.createElement("button");
 backButton.setAttribute("id", "backButton");
 backButton.innerText = "<Back";
 backButton.addEventListener("click", function () {
-            window.location.href="index.html"
-            })
-cardSolo.appendChild(backButton);      
+  back();
+});
+cardSolo.appendChild(backButton);
 
 document
-  .querySelectorAll('[data-tiny-editor]')
-  .forEach(editor =>
-    editor.addEventListener('input', e => console.log(e.target.innerHTML)
-  )
-);
+  .querySelectorAll("[data-tiny-editor]")
+  .forEach((editor) =>
+    editor.addEventListener("input", (e) => console.log(e.target.innerHTML))
+  );
 
+async function back() {
+  window.location.href = "index.html";
+}
 
+async function editor() {
+  const prevMage = document.getElementById("prevmage");
+  let text = Array.from(document.querySelectorAll("textarea"));
+  let values = text.map(({ value }) => {
+    return value.trim();
+  });
+  if (values.some((value) => value === "")) {
+    alert("Please fill in all fields");
+    return;
+  }
 
+  let [name, shortDescription] = values;
+  console.log(values);
+  let image = prevMage.src;
+  image = image.substr(22);
+  console.log(image);
+  let id = idChara;
+  let description = document.getElementById("editor").innerHTML;
+  console.log(description);
 
- 
+  const postData = await fetch(
+    "https://character-database.becode.xyz/characters/" + id,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, shortDescription, description, image }),
+    }
+  );
+  console.log(postData);
+  console.log(postData.json());
+  back();
+}
--- upchara.js
+++ upchara.js
@@ -1,156 +1,135 @@
-    let idChara = sessionStorage.getItem("idChara")
-    let nameChara =sessionStorage.getItem("nameChara")
-    let descriptionChara = sessionStorage.getItem("descriptionChara")
-    let shortDesChara = sessionStorage.getItem("shortDesChara")
-    let imgChara = sessionStorage.getItem("imgChara")
-    console.log(idChara);
-    console.log(nameChara);
-    console.log(descriptionChara);
-    console.log(shortDesChara);
-    console.log(imgChara);
+let idChara = sessionStorage.getItem("idChara");
+let nameChara = sessionStorage.getItem("nameChara");
+let descriptionChara = sessionStorage.getItem("descriptionChara");
+let shortDesChara = sessionStorage.getItem("shortDesChara");
+let imgChara = sessionStorage.getItem("imgChara");
+console.log(idChara);
+console.log(nameChara);
+console.log(descriptionChara);
+console.log(shortDesChara);
+console.log(imgChara);
 
-    document.getElementById("name").innerHTML = nameChara;
-    document.getElementById("shortDescription").innerHTML = shortDesChara;
-    document.getElementById("editor").innerHTML = descriptionChara;
-    
+document.getElementById("name").innerHTML = nameChara;
+document.getElementById("shortDescription").innerHTML = shortDesChara;
+document.getElementById("editor").innerHTML = descriptionChara;
 
-
-var // where files are dropped + file selector is opened
-	dropRegion = document.getElementById("drop-region"),
-	// where images are previewed
-	imagePreviewRegion = document.getElementById("image-preview");
-
+let // where files are dropped + file selector is opened
+  dropRegion = document.getElementById("drop-region"),
+  // where images are previewed
+  imagePreviewRegion = document.getElementById("image-preview");
 
 // open file selector when clicked on the drop region
-var fakeInput = document.createElement("input");
+let fakeInput = document.createElement("input");
 fakeInput.type = "file";
 fakeInput.accept = "image/*";
 fakeInput.multiple = true;
-dropRegion.addEventListener('click', function() {
-	fakeInput.click();
+dropRegion.addEventListener("click", function () {
+  fakeInput.click();
 });
 
-
-fakeInput.addEventListener("change", function() {
-	var files = fakeInput.files;
-	handleFiles(files);
+fakeInput.addEventListener("change", function () {
+  let files = fakeInput.files;
+  handleFiles(files);
 });
-
 
 function preventDefault(e) {
-	e.preventDefault();
-  	e.stopPropagation();
+  e.preventDefault();
+  e.stopPropagation();
 }
 
-dropRegion.addEventListener('dragenter', preventDefault, false)
-dropRegion.addEventListener('dragleave', preventDefault, false)
-dropRegion.addEventListener('dragover', preventDefault, false)
-dropRegion.addEventListener('drop', preventDefault, false)
-
+dropRegion.addEventListener("dragenter", preventDefault, false);
+dropRegion.addEventListener("dragleave", preventDefault, false);
+dropRegion.addEventListener("dragover", preventDefault, false);
+dropRegion.addEventListener("drop", preventDefault, false);
 
 function handleDrop(e) {
-	var dt = e.dataTransfer,
-		files = dt.files;
+  let dt = e.dataTransfer,
+    files = dt.files;
 
-	if (files.length) {
+  if (files.length) {
+    handleFiles(files);
+  } else {
+    // check for img
+    let html = dt.getData("text/html"),
+      match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html),
+      url = match && match[1];
 
-		handleFiles(files);
-		
-	} else {
+    if (url) {
+      uploadImageFromURL(url);
+      return;
+    }
+  }
 
-		// check for img
-		var html = dt.getData('text/html'),
-	        match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html),
-	        url = match && match[1];
+  function uploadImageFromURL(url) {
+    let img = new Image();
+    let c = document.createElement("canvas");
+    let ctx = c.getContext("2d");
 
+    img.onload = function () {
+      c.width = this.naturalWidth; // update canvas size to match image
+      c.height = this.naturalHeight;
+      ctx.drawImage(this, 0, 0); // draw in image
+      c.toBlob(function (blob) {
+        // get content as PNG blob
 
-
-	    if (url) {
-	        uploadImageFromURL(url);
-	        return;
-	    }
-
-	}
-
-
-	function uploadImageFromURL(url) {
-		var img = new Image;
-        var c = document.createElement("canvas");
-        var ctx = c.getContext("2d");
-
-        img.onload = function() {
-            c.width = this.naturalWidth;     // update canvas size to match image
-            c.height = this.naturalHeight;
-            ctx.drawImage(this, 0, 0);       // draw in image
-            c.toBlob(function(blob) {        // get content as PNG blob
-
-            	// call our main function
-                handleFiles( [blob] );
-
-            }, "image/png");
-        };
-        img.onerror = function() {
-            alert("Error in uploading");
-        }
-        img.crossOrigin = "";              // if from different origin
-        img.src = url;
-	}
-
+        // call our main function
+        handleFiles([blob]);
+      }, "image/png");
+    };
+    img.onerror = function () {
+      alert("Error in uploading");
+    };
+    img.crossOrigin = ""; // if from different origin
+    img.src = url;
+  }
 }
 
-dropRegion.addEventListener('drop', handleDrop, false);
-
-
+dropRegion.addEventListener("drop", handleDrop, false);
 
 function handleFiles(files) {
-	for (var i = 0, len = files.length; i < len; i++) {
-		if (validateImage(files[i]))
-			previewImage(files[i]);
-	}
+  for (let i = 0, len = files.length; i < len; i++) {
+    if (validateImage(files[i])) previewImage(files[i]);
+  }
 }
 
 function validateImage(image) {
-	// check the type
-	var validTypes = ['image/jpeg', 'image/png', 'image/gif'];
-	if (validTypes.indexOf( image.type ) === -1) {
-		alert("Invalid File Type");
-		return false;
-	}
+  // check the type
+  let validTypes = ["image/jpeg", "image/png", "image/gif"];
+  if (validTypes.indexOf(image.type) === -1) {
+    alert("Invalid File Type");
+    return false;
+  }
 
-	// check the size
-	var maxSizeInBytes = 10e6; // 10MB
-	if (image.size > maxSizeInBytes) {
-		alert("File too large");
-		return false;
-	}
+  // check the size
+  let maxSizeInBytes = 10e6; // 10MB
+  if (image.size > maxSizeInBytes) {
+    alert("File too large");
+    return false;
+  }
 
-	return true;
-
+  return true;
 }
 
 // container
-var imgView = document.createElement("div");
+const imgView = document.createElement("div");
 imgView.className = "image-view";
 imagePreviewRegion.appendChild(imgView);
 
 // previewing image
-var img = document.createElement("img");
-img.setAttribute("id","prevmage");
-img.className="rounded";
+const img = document.createElement("img");
+img.setAttribute("id", "prevmage");
+img.className = "rounded";
 imgView.appendChild(img);
 img.src = imgChara;
 
 function previewImage(image) {
-	// read the image...
-	var reader = new FileReader();
-	reader.onload = function(e) {
-		img.src = e.target.result;
+  // read the image...
+  let reader = new FileReader();
+  reader.onload = function (e) {
+    img.src = e.target.result;
   };
-	reader.readAsDataURL(image);
+  reader.readAsDataURL(image);
 }
-
-
-
 
 const buttonSolo = document.createElement("div");
 buttonSolo.setAttribute("class", "buttonSolo");
@@ -159,76 +138,92 @@ cardSolo.appendChild(buttonSolo);
 const updateButton = document.createElement("input");
 updateButton.setAttribute("id", "updateButton");
 updateButton.setAttribute("type", "submit");
-updateButton.setAttribute("value", "Save New Character");;
+updateButton.setAttribute("value", "Save Updated Character");
 buttonSolo.appendChild(updateButton);
-updateButton.addEventListener("click",async() =>{
-    const prevMage = document.getElementById("prevmage");
-    let text = Array.from(document.querySelectorAll("textarea"));
-    let values = text.map(({value})=>{
-        return value.trim();
-    });
-    if (values.some((value) => value === "")) {
-        alert("Please fill in all fields");
-        return;
-      }
-  
-      let [name, shortDescription] = values;
-      console.log(values);
-      let image = prevMage.src;
-      image = image.substr(22);
-      console.log(image);
-      let id = idChara;
-      let description = document.getElementById("editor").innerHTML;
-      console.log (description);
- 
-  
-      const postData = await fetch("https://character-database.becode.xyz/characters/"+id, {
-        method: "PUT",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, shortDescription, description, image }),
-      });
-      console.log(postData);
-      console.log(postData.json());
-      window.location.href="index.html"
-    });
-  
-  
-
+updateButton.addEventListener("click", async () => {
+  editor();
+});
 
 const deleteButton = document.createElement("button");
 deleteButton.setAttribute("id", "deleteButton");
 deleteButton.innerText = "Delete character";
 buttonSolo.appendChild(deleteButton);
 deleteButton.addEventListener("click", () => {
- back();
-            });
-
+  deleteChara();
+});
 
 const backButton = document.createElement("button");
 backButton.setAttribute("id", "backButton");
 backButton.innerText = "<Back";
 backButton.addEventListener("click", () => {
- back();
-            })
-cardSolo.appendChild(backButton);          
-
+  back();
+});
+cardSolo.appendChild(backButton);
 
 document
-  .querySelectorAll('[data-tiny-editor]')
-  .forEach(editor =>
-    editor.addEventListener('input', e => console.log(e.target.innerHTML)
-  )
-);
+  .querySelectorAll("[data-tiny-editor]")
+  .forEach((editor) =>
+    editor.addEventListener("input", (e) => console.log(e.target.innerHTML))
+  );
 
 async function back() {
-    window.location.href="index.html"
+  window.location.href = "index.html";
+}
+
+async function deleteChara() {
+  if (confirm("Are you sure?") == true) {
+    try {
+      let resp = await fetch(
+        "https://character-database.becode.xyz/characters/" + idChara,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!resp.ok) {
+        throw new Error("ID not found");
+      } else {
+        window.location.href = "index.html";
+      }
+    } catch (err) {
+      alert(err);
+    }
   }
+}
 
+async function editor (){
+const prevMage = document.getElementById("prevmage");
+let text = Array.from(document.querySelectorAll("textarea"));
+let values = text.map(({ value }) => {
+  return value.trim();
+});
+if (values.some((value) => value === "")) {
+  alert("Please fill in all fields");
+  return;
+}
 
+let [name, shortDescription] = values;
+console.log(values);
+let image = prevMage.src;
+image = image.substr(22);
+console.log(image);
+let id = idChara;
+let description = document.getElementById("editor").innerHTML;
+console.log(description);
 
-
-
-
- 
+const postData = await fetch(
+  "https://character-database.becode.xyz/characters/" + id,
+  {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, shortDescription, description, image }),
+  }
+);
+console.log(postData);
+console.log(postData.json());
+back();
+}
